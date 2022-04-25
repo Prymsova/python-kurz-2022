7aa568740c22b8e6bbb15326702a777637bd5bfc
Anna Prymšová <anca7@centrum.cz>
2022-04-25 18:38:54 +0200

    python2022_ukol_03_Prymsova

--- ukol_03.py
+++ ukol_03.py
@@ -1,24 +1,21 @@
-tel_cislo = input("Zadejte telefonní číslo, na které chcete zaslat SMS zprávu:")
+cislo_prijemce = input("Vložte telefonní číslo: ")
 
-def kontrola_cisla():
-    delka_cisla = len(tel_cislo)
-
-    if delka_cisla == 9 or (delka_cisla == 13 and "+420" in tel_cislo):
+def overeni_platnosti_cisla(tel_cislo):
+    tel_cislo = tel_cislo.replace(" ", "")
+    if len(tel_cislo) == 9 or (len(tel_cislo) == 13 and tel_cislo[0:4] == "+420" in tel_cislo):
         return True
     else:
         return False
 
-kontrola_cisla()
+def vypocet_ceny_zpravy(text_zpravy:str, delka_sms:int=180, cena_sms:int=3):
+    pocet_sms = len(text_zpravy) // delka_sms + 1
+    cena_zpravy = pocet_sms * cena_sms
+    return cena_zpravy
 
-def vypocet_ceny(pocet_zprav, cena_SMS=3):
-    cena_zpravy = pocet_zprav * cena_SMS
-    print(f"Vaše zpráva byla odeslána. Cena Vaší zprávy je {cena_zpravy} Kč.")
 
-#nefungovalo bez závorek
-if kontrola_cisla():
-    text_zpravy = input("Napište text SMS zprávy, kterou chcete odeslat:")
-    pocet_zprav = len(text_zpravy) // 180 + 1
-    vypocet_ceny(pocet_zprav)
-
+if overeni_platnosti_cisla(cislo_prijemce):
+    print("číslo je platné")
+    text_zpravy = input("Vložte zprávu, kterou chcete poslat: ")
+    print(f"Cena Vaší zprávy je {vypocet_ceny_zpravy(text_zpravy)} Kč.")
 else:
-    print(f"Chyba formátu čísla - prosím, ověřte si správnost telefonního čísla.")
+    print("vložené číslo je neplatné")
